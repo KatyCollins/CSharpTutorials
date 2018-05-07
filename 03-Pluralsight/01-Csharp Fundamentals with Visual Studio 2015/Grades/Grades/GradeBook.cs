08d964c8f1ce0630c1b4fff9770582848a4b6744
Katy Collins <katy.collins1@gmail.com>
2018-05-07 12:20:33 -0500

    Initial pluralsight gradebook app

---
+++ 03-Pluralsight/01-Csharp Fundamentals with Visual Studio 2015/Grades/Grades/GradeBook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        // ctor <tab><tab> creates blank constructor
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+            foreach(float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        // if public is not defined, it is assumed private in C#
+        private List<float> grades;
+    }
+}
